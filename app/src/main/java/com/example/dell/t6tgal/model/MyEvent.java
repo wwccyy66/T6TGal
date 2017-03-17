8211a5ac883c083f3cc9f9ade49b140b0e9bd5ba
wwccyy66 <463044107@qq.com>
2017-03-17 12:11:40 +0800

    给赵总的信息

---
+++ app/src/main/java/com/example/dell/t6tgal/model/MyEvent.java
@@ -0,0 +1,28 @@
+package com.example.dell.t6tgal.model;
+
+/**
+ * Created by dell on 2017/3/16.
+ */
+
+public class MyEvent {
+
+    private String type;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    private String content;
+}
